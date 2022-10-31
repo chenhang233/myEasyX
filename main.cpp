0ccb675fe8989e29c188dbaa44508c50ca74c1ea
ChenHang <chenhang223333@163.com>
2022-10-31 11:13:14 +0800

    feat:人物初始化,渲染

--- main.cpp
+++ main.cpp
@@ -6,8 +6,12 @@
 #define WIN_HEIGHT 396
 
 IMAGE image_bg[3];
+IMAGE image_hero[12];
 int bgX[3];			
 int bgSpeed[3] = { 1,2,4 };
+int heroX;
+int heroY;
+int heroIndex;
 
 void init() {
 	initgraph(WIN_WIDTH, WIN_HEIGHT);
@@ -19,6 +23,14 @@ void init() {
 		loadimage(&image_bg[i], name);
 		bgX[i] = 0;
 	}
+	for (int i = 0; i < 12; i++)
+	{
+		sprintf(name, "material/res/res/hero%d.png", i + 1);
+		loadimage(&image_hero[i], name);
+		heroX = WIN_WIDTH / 2 - image_hero[i].getwidth() / 2;
+		heroY = 345 - image_hero[i].getheight();
+		heroIndex = 0;
+	}
 }
 
 void fly() {
@@ -28,6 +40,7 @@ void fly() {
 			bgX[i] = 0;
 		bgX[i] -= bgSpeed[i];
 	}
+	heroIndex = (heroIndex + 1) % 12;
 }
 void updateBg() {
 	putimagePNG2(bgX[0], 0,&image_bg[0]);
@@ -43,8 +56,10 @@ int main()
 		// 更新背景
 		BeginBatchDraw();// 让 putimagePNG2不一条条打印,而是先缓存好所以片段,再打印,解决闪烁问题
 		updateBg();
+		putimagePNG2(heroX, heroY, &image_hero[heroIndex]);
 		EndBatchDraw();
 		fly();
+		Sleep(30);
 	}
 	system("pause");
 
